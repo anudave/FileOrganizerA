eb5299ecb809785e15d3d06cb1153ac9683707e7
anwar dawud <anwardawud178@gmail.com>
2026-06-01 13:27:35 +0300

    migration: Remove CloudOrganizationLogs and CloudStorageAccounts tables
    - Drop obsolete cloud storage tables
    - Simplify database schema
    - Remove unused cloud integration support

--- WpfApp1/Data/Migrations/20260601102136_RemoveCloudOrganizationTables.Designer.cs
+++ WpfApp1/Data/Migrations/20260601102136_RemoveCloudOrganizationTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WpfApp1.Data;
 
@@ -10,9 +11,11 @@ using WpfApp1.Data;
 namespace WpfApp1.Data.Migrations
 {
     [DbContext(typeof(FileOrganizerContext))]
-    partial class FileOrganizerContextModelSnapshot : ModelSnapshot
+    [Migration("20260601102136_RemoveCloudOrganizationTables")]
+    partial class RemoveCloudOrganizationTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
